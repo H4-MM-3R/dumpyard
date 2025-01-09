c8a73b2194c62ce6a59a995dd2b100b3f743b581
H4-MM-3R <kuheram777@gmail.com>
2025-01-09 23:50:23 +0530

    Update 09/01/2025: Arrays - done few Medium

--- sheets/arrays/medium.cpp
+++ sheets/arrays/medium.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
-#include <array>
 #include <climits>
 #include <iostream>
+#include <unordered_set>
+#include <utility>
 #include <vector>
+
 
 using namespace std;
 
@@ -106,7 +108,7 @@ int solve3(vector<int> arr) {
 }
 int solve3b(vector<int> arr) {
   // Moore's Voting Algorithm
-  
+
   int maj_ele = 0;
   int maj_cnt = 0;
   for (int ele : arr) {
@@ -124,46 +126,217 @@ int solve3b(vector<int> arr) {
  * 4. Maximum subarray sum in an array
  */
 int solve4(vector<int> arr) {
-    // Kadane's Algorithm
-   
-    int maxi = INT_MIN;
-    int sum = 0;
-    for(int ele: arr){
-        sum += ele;
+  // Kadane's Algorithm
 
-        if(sum > maxi) maxi = sum;
-        if(sum < 0) sum = 0;
-    }
-    return maxi;
+  int maxi = INT_MIN;
+  int sum = 0;
+  for (int ele : arr) {
+    sum += ele;
+
+    if (sum > maxi)
+      maxi = sum;
+    if (sum < 0)
+      sum = 0;
+  }
+  return maxi;
 }
 
 int solve4b(vector<int> arr) {
-    // Follow up: There might be more than one subarray that has the largest sum. so print any of the subarrays too.
-    int maxi = INT_MIN;
-    int sum = 0, ptr = 0, start = -1, end = -1;
+  // Follow up: There might be more than one subarray that has the largest sum.
+  // so print any of the subarrays too.
+  int maxi = INT_MIN;
+  int sum = 0, ptr = 0, start = -1, end = -1;
 
-    for(int i = 0; i < arr.size(); i++){
-        if(sum == 0) ptr = i;
-        sum += arr[i];
+  for (int i = 0; i < arr.size(); i++) {
+    if (sum == 0)
+      ptr = i;
+    sum += arr[i];
 
-        if(sum > maxi){
-            maxi = sum;
-            start = ptr;
-            end = i;
-        }
-
-        if(sum < 0) sum = 0;
+    if (sum > maxi) {
+      maxi = sum;
+      start = ptr;
+      end = i;
     }
-    cout << "[";
-    for(int i = start; i <= end; i++){
-        cout << arr[i] << " ";
-    }
-        cout << "]\n";
-    return maxi;
+
+    if (sum < 0)
+      sum = 0;
+  }
+  cout << "[";
+  for (int i = start; i <= end; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "]\n";
+  return maxi;
 }
 
+/*
+ * 5. Stock buy and sell
+ */
+int solve5(vector<int> arr) {
+  int max_profit = 0;
+  int min_price = INT_MAX;
+  for (int ele : arr) {
+    min_price = min(min_price, ele);
+    max_profit = max(max_profit, ele - min_price);
+  }
+  return max_profit;
+}
+
+/*
+ * 6. Rearrange array elements by alternating signs
+ */
+vector<int> solve6(vector<int> &arr) {
+  int n = arr.size();
+  vector<int> ans(n, 0);
+  int pos_ptr = 0, neg_ptr = 1;
+  for (int i = 0; i < n; i++) {
+    if (arr[i] < 0) {
+      ans[neg_ptr] = arr[i];
+      neg_ptr += 2;
+    } else {
+      ans[pos_ptr] = arr[i];
+      pos_ptr += 2;
+    }
+  }
+  return ans;
+}
+vector<int> solve6b(vector<int> &arr) {
+  // variation: if no of negative and positive elements are unequal
+    vector<int> pos;
+    vector<int> neg;
+
+    for(int ele : arr){
+        if(ele < 0) neg.push_back(ele);
+        else pos.push_back(ele);
+    }
+    int pos_index = pos.size();
+    int neg_index = neg.size();
+
+    if(pos_index < neg_index){
+        for(int i = 0; i < pos_index; i++){
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+
+        int index = pos_index * 2;
+        for(int i = pos_index; i < neg_index; i++){
+            arr[index] = neg[i];
+            index++;
+        }
+    } else {
+        for(int i = 0; i < neg_index; i++){
+            arr[2*i] = pos[i];
+            arr[2*i+1] = neg[i];
+        }
+
+        int index = neg_index * 2;
+        for(int i = neg_index; i < pos_index; i++){
+            arr[index] = pos[i];
+            index++;
+        }
+    }
+
+    return arr;
+}
+
+/*
+ * 7. Find the next lexicographically greater permutation
+ */
+vector<int> solve7(vector<int> &arr){
+    int len = arr.size();
+
+    int idx = -1;                           // index of the Pivot
+                  
+                  
+    for(int i = len - 2; i >= 0; i--){     // find the Pivot
+        if(arr[i] > arr[i + 1]){
+            idx = i;
+            break;
+        }
+    }
+
+    if(idx == -1){
+        reverse(arr.begin(), arr.end());    // if no Pivot found, reverse the array (largest permutation)
+        return arr;
+    }
+
+    for(int i = len - 1; i > idx; i--){    // swap pivot with the next greatest element.
+        if(arr[i] > arr[idx]){
+            swap(arr[i], arr[idx]);
+            break;
+        }
+    }
+
+    reverse(arr.begin(), arr.end());
+
+    return arr;
+}
+
+/*
+ * 8. Print all the " Leader " Elements
+ *
+ * Leader: element greater than all the 
+ */
+
+vector<int> solve8(vector<int> arr){
+    vector<int> ans;
+
+    int len = arr.size();
+    ans.push_back(arr[len - 1]);
+    for(int i = len - 2; i > 0; i--){
+        if(ans.back() < arr[i]){
+            ans.push_back(arr[i]);
+        }
+    }
+    return ans;
+}
+
+/*
+ * 9. Find the length of the longest sequence which contains the consecutive elements.
+ */
+int solve9(vector<int> arr){
+    sort(arr.begin(), arr.end());
+    int last_smaller = INT_MIN;
+    int cnt = 0;
+    int ans = 1;
+
+    for(int ele: arr){
+        if(ele - 1 == last_smaller){
+            cnt++;
+            last_smaller = ele;
+        } else if(ele != last_smaller){
+            last_smaller = ele;
+            cnt = 1;
+        }
+        ans = max(ans, cnt);
+    }
+    return ans;
+
+}
+
+int solve9b(vector<int> arr){
+    // using hashing
+
+    unordered_set<int> st;
+
+    for(int ele: arr) st.insert(ele);
+
+    int ans = 1;
+    for(int ele: arr){                      // ele - 1 is the previous consecutive element
+        if(st.find(ele - 1) == st.end()){   // ele - 1 doesn't exist in set then it might be 
+            int cnt = 1;                    // the first consecutive element
+            int x = ele;
+            while(st.find(x + 1) != st.end()){
+                x++;
+                cnt++;
+            }
+            ans = max(ans, cnt);
+        }
+    }
+    return ans;
+}
 
 int main() {
-  vector<int> arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
-  cout << solve4b(arr);
+    vector<int> arr = {100, 200, 1, 2, 3, 4};
+    cout << solve9b(arr); 
 }
